c691219a5fc1a47fd46cf5f460fcd773ff37d6e0
dannyseymour <daniel@discordia.io>
2019-10-31 10:36:23 -0600

    added dialog fragment

--- app/src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseFragment.java
+++ app/src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseFragment.java
@@ -1,7 +1,12 @@
 package edu.cnm.deepdive.diceware.controller;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,13 +21,66 @@ import edu.cnm.deepdive.diceware.model.Passphrase;
  * to create an instance of this fragment.
  */
 public class PassphraseFragment extends DialogFragment {
+  private OnCompleteListener listener;
+  private Passphrase passphrase;
 
+
+  public static PassphraseFragment newInstance(){
+    return newInstance(null);
+  }
+
+  public OnCompleteListener getListener() {
+    return listener;
+  }
+
+  public void setListener(
+      OnCompleteListener listener) {
+    this.listener = listener;
+  }
+
+  public Passphrase getPassphrase() {
+    return passphrase;
+  }
+
+  public void setPassphrase(Passphrase passphrase) {
+    this.passphrase = passphrase;
+  }
+
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    Passphrase temp = (Passphrase) getArguments().getSerializable("passphrase");
+    Passphrase passphrase = (temp!=null) ? temp: new Passphrase();
+    View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_passphrase2, null);
+    EditText passphraseKey = view.findViewById(R.id.passphrase_key);
+    if (passphrase.getKey()!= null){
+      passphraseKey.setText(passphrase.getKey());
+    }
+    return new AlertDialog.Builder(getContext())
+        .setTitle("Passphrase Details")
+        .setView(view)
+        .setNegativeButton("Cancel",(dialog,button)->{})
+        .setPositiveButton("Ok",(dialog, button)->{
+          if (listener != null) {
+            passphrase.setKey(passphraseKey.getText().toString());
+            listener.complete(passphrase);
+          }
+        })
+      .create();
+  }
 
   public static PassphraseFragment newInstance(Passphrase passphrase) {
     PassphraseFragment fragment = new PassphraseFragment();
     Bundle args = new Bundle();
+    if(passphrase != null) {
+      args.putSerializable("passphrase", passphrase);
+    }
     fragment.setArguments(args);
     return fragment;
+  }
+  @FunctionalInterface
+  public interface OnCompleteListener {
+    void complete(Passphrase passphrase);
   }
 
 }
